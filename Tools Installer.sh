f5075054d70d52e59bd74fff777553e9d4f229c5
tidaktahu <44739049+tidaktahu@users.noreply.github.com>
2018-11-04 10:26:30 +0700

    ToolsInstaller

---
+++ Tools Installer.sh
@@ -0,0 +1,630 @@
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+figlet "MR.UNKNOW"|lolcat
+
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [  Author :  MR.UNKNOW / TIDAKDIKETAH.GIT       ]"
+echo "              [===============================================]"|lolcat
+echo "              [  TEAM   :    NO TEAM:TEAM NUB                 ]"
+echo "              [  WA     :    0858777123977                    ]"
+echo "              [===============================================]"|lolcat
+echo "              [        GUNAKAN TOOLS INI DENGAN BIJAK         ]"
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [+] Mr UNKNOW | TIDAK DIKETAHUI| NOTEAM IS ME [+]"|lolcat
+echo "              [===============================================]"|lolcat
+
+echo "================================="|lolcat
+echo " 1.Crack FB "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 2.Dump cc with sqlmap "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 3.Cari web vuln sql "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 4.AOC DEFACE "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 5.Black hydra "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 6.WEEMAN [PHISING] "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 7.LITESPAM [BOM SMS PACAR LU YE]"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 8.NUYUL CASHTREE"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 9.BRUTE FB [WORDLIST]"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 10.BOBOL WIFI"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 11.BOT FACEBOOK"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 12.TOOLS BLAZY"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 13.REDHAWK (SCAN WEB)"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 14.xNot_Found Tools"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 15.HAMMER [DDOS]"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 16.SPAM BUKALAPAK (WA)"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 17.TUAN B4DUT INSTALLER"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 18.Hash-Buster"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 19.REMOTE SHELL"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 20.INSTABOT"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 21.EMAILHARVERTER"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 22.BLACKNMAP"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 23.NMAP"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 24.Lite DDOS"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 25.CRACK PASSWORD HASH"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 26.BUSCA PANEL ADMIN"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 27.VISQL"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 28.LAZYMUX"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 29.Mr.cakil"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 30.PHISING ML (MOBA ANALOG)"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 31.TU4N N1CKN4M3 V1"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 32.FAKE ROOT ^_^"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 33.BUAT SCRIPT DEFACE"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 34.KICKTHEMOUT "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 35.BUAT LOGIN DI TERMUX"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 36.CHATTING ALA HACKER"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 37.JOOMSCAN"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 38.WORDPRESSCAN"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 39.WPSeku"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 40.DEVLOIT"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 41.SHELL-SCAN"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 42.WEBKILLER"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 43.HUNNER FRAMEWORK"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 44.KALI NETHUNTER"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 45.SirKz0L  passtolsv1"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 46.Fsociety"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 47.Crewbot"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 48.SANTET ONLINE"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 49.WIFIPHISER BOBOL WIFI"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 50.install ngrok"
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 51.NuyuL kubik "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 52.METASPLOIT "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo " 00.KELUAR DAN TINGGALKAN MASALALU"
+echo "================================="|lolcat
+trap ctrl_c INT
+ctrl_c()
+{
+echo -e "$red(Ctrl + C ) SEGITU DULU YE NJING "
+figlet -f digital "     BYEEE"
+sleep 1
+exit
+}
+read -p "Silahkan Pilih :" gg
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+pkg update -y
+pkg install python2  -y
+pkg install git -y
+pip2 install mechanize
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+cd multi-bruteforce-facebook
+echo "GASS MBF DAH SIAP" | lolcat
+sleep 1
+python2 MBF.py
+fi
+if [ $gg = 2 ] || [ $gg = 2 ]
+then
+clear
+pkg update -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+echo "gunakan -u (masukin target) --dbs (nyari database) --T (cari table)"|l$
+sleep 1
+python2 sqlmap.py
+fi
+if [ $gg = 3 ] || [ $gg = 3 ]
+then
+figlet -f smslant "TOOLS V2"|lolcat
+apt upgrade -y
+apt install git -y
+apt install python2 -y
+git clone https://github.com/CiKu370/ko-dork
+cd ko-dork
+sleep 1
+python2 dork.py
+fi
+if [ $gg = 4 ] || [ $gg = 4 ]
+then
+figlet -f smslant "TOOLS V2"|lolcat
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sleep 1
+sh AOC.sh
+fi
+if [ $gg = 5 ] || [ $gg = 5 ]
+then
+pkg install python2 -y
+git clone https://github.com/Gameye98/Black-Hydra
+cd Black-Hydra
+chmod +x blackhydra.py
+sleep 1
+python2 blackhydra.py
+fi
+if [ $gg = 6 ] || [ $gg = 6 ]
+then
+pkg install git -y
+git clone https://github.com/samyoyo/weeman
+chmod +x weeman
+cd weeman
+sleep 1
+python2 weeman.py
+fi
+if [ $gg = 7 ] || [ $gg = 7 ]
+then
+apt update && apt upgrade -y
+apt install git
+apt install php -y
+apt install sh
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM/
+sleep1
+sh LITESPAM.sh
+fi
+if [ $gg = 8 ] || [ $gg = 8 ]
+then
+clear
+git clone https://github.com/radenvodka/cashtree/
+cd cashtree
+sleep1
+nano cashtree.php
+echo "UDAH ke nano? ketik ini php cashtree.php"|lolcat
+fi
+if [ $gg = 9 ] || [ $gg = 9 ]
+then
+clear
+pip2 install mechanize
+apt install git
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+sleep1
+python2 jomblo.py
+fi
+if [ $gg = 10 ] || [ $gg = 10 ]
+then
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+sleep1
+sh wifi-hacker.sh
+fi
+if [ $gg = 11 ] || [ $gg = 11 ]
+then
+apt install git php curl -y
+git clone http://github.com/AMVengeance/FB-React.git
+cd FB-React
+chmod +x start
+sleep1
+./start
+fi
+if [ $gg = 12 ] || [ $gg = 12 ]
+then
+apt install git python2 python2-dev clang libxml2 -y
+git clone https://github.com/UltimateHackers/Blazy.git
+cd Blazy
+pip2 install -r requirements.txt
+cd Blazy
+sleep1
+python2 blazy.py
+fi
+if [ $gg = 13 ] || [ $gg = 13 ]
+then
+apt install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+sleep1
+php rhawk.php
+fi
+if [ $gg = 14 ] || [ $gg = 14 ]
+then
+git clone https://github.com/hatakecnk/xNot_Found.git
+cd xNot_Found
+cat README.md
+chmod +x xNot_Found.sh
+sh xNot_Found.sh
+fi
+if [ $gg = 15 ] || [ $gg = 15 ]
+then
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python3 hammer.py
+fi
+if [ $gg = 16 ] || [ $gg = 16 ]
+then
+git clone https://github.com/siputra12/prank
+cd prank
+ls
+php wa.php
+fi
+if [ $gg = 17 ] || [ $gg = 17 ]
+then
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+sh TUANB4DUT.sh
+fi
+if [ $gg = 18 ] || [ $gg = 18 ]
+then
+pkg install git
+git clone https://github.com/UltimateHackers/Hash-Buster/ hash-b/
+cd hash-b
+python2 hash.py
+fi
+if [ $gg = 19 ] || [ $gg = 19 ]
+then
+apt install perl
+git clone https://github.com/aryanrtm/Remote-Shell
+cd Remote-Shell
+perl remoteshell.pl
+fi
+if [ $gg = 20 ] || [ $gg = 20 ]
+then
+apt install git
+git clone https://github.com/verluchie/instabot.py
+cd instabot.py
+pip2 install -r requirements.txt
+python2 example.py
+fi
+if [ $gg = 21 ] || [ $gg = 21 ]
+then
+git clone https://github.com/maldevel/EmailHarvester
+cd EmailHarvester
+pip2 install -r requirements.txt
+python2 EmailHarvester.py
+fi
+if [ $gg = 22 ] || [ $gg = 22 ]
+then
+git clone https://github.com/Xi4u7/BlackNmap
+cd BlackNmap
+chmod 777 blacknmap
+./blacknmap
+fi
+if [ $gg = 23 ] || [ $gg = 23 ]
+then
+apt install git
+git clone https://github.com/Xi4u7/NMapForAndroid
+cd NMapForAndroid
+sh install.sh
+fi
+if [ $gg = 24 ] || [ $gg = 24 ]
+then
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+fi
+if [ $gg = 25 ] || [ $gg = 25 ]
+then
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+fi
+if [ $gg = 26 ] || [ $gg = 26 ]
+then
+pkg install git
+git clone https://github.com/Techzindia/admin_penal
+cd admin_penal
+chmod +x admin_panel_finder.py
+python2 admin_panel_finder.py
+fi
+if [ $gg = 27 ] || [ $gg = 27 ]
+then
+apt install git
+git clone https://github.com/blackvkng/viSQL
+cd viSQL
+python2 -m pip install -r requirements.txt
+python2 viSQL.py
+python2 viSQL.py -t
+fi
+if [ $gg = 28 ] || [ $gg = 28 ]
+then
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+fi
+if [ $gg = 29 ] || [ $gg = 29 ]
+then
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+fi
+if [ $gg = 30 ] || [ $gg = 30 ]
+then
+apt install git
+git clone https://github.com/Senitopeng/PhisingGame.git
+cd PhisingGame
+python2 phising.py
+fi
+if [ $gg = 31 ] || [ $gg = 31 ]
+then
+pip2 install lolcat
+git clone https://github.com/TU4NN1CKN4M3/Trouble_shoot
+cd Trouble_shoot
+sh GASSKUY.sh
+fi
+if [ $gg = 32 ] || [ $gg = 32 ]
+then
+wget http://lexiie.web.id/Download/Fakeroot_1.1_all.deb
+sleep1
+apt install -y ./Fakeroot_1.1_all.deb
+fi
+if [ $gg = 33 ] || [ $gg = 33 ]
+then
+apt install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-daface-creator
+chmod +x create.py
+python2 create.py
+fi
+if [ $gg = 34 ] || [ $gg = 34 ]
+then
+git clone https://github.com/k4m4/kickthemout.git
+cd kickthemout
+chmod + x kickthemout.py
+pip2 install -r requirement.txt
+python2 kickthemout.py
+fi
+if [ $gg = 35 ] || [ $gg = 35 ]
+then
+git clone https://github.com/Harisgitama/termux-loginv2fx.git
+cd termux-loginv2fx
+python2 setup.py
+cd $HOME
+python2 useradd.py
+fi
+if [ $gg = 36 ] || [ $gg = 36 ]
+then
+apt-get update && apt-get install irssi
+irssi
+/connect irc.freenode.net
+/nick kamu
+/join
+fi
+if [ $gg = 37 ] || [ $gg = 37 ]
+then
+git clone https://github.com/rezasp/joomscan.git
+cd joomscan
+perl joomscan.pl
+fi
+if [ $gg = 38 ] || [ $gg = 38 ]
+then
+git clone https://github.com/swisskyrepo/Wordpresscan
+cd Wordpresscan
+pip2 install -r requirements.txt
+python2 main.py
+fi
+if [ $gg = 39 ] || [ $gg = 39 ]
+then
+git clone https://github.com/m4ll0k/WPSeku
+cd WPSeku
+chmod 777 wpseku.py
+pip2 install -r requirements.txt
+python2 wpseku.py -h
+fi
+if [ $gg = 40 ] || [ $gg = 40 ]
+then
+git clone https://github.com/joker25000/Devploit
+cd Devploit
+chmod +x install
+./install
+python2 Devploit
+fi
+if [ $gg = 41 ] || [ $gg = 41 ]
+then
+git clone https://github.com/Rhi7/shell-scan
+cd shell-scan
+chmod +x shell.py
+python shell.py -u example -w wordlist.txt
+fi
+if [ $gg = 42 ] || [ $gg = 42 ]
+then
+git clone https://github.com/ultrasecurity/webkiller.git
+cd webkiller
+pip install -r requirements.txt
+python webkiller.py
+fi
+if [ $gg = 43 ] || [ $gg = 43 ]
+then
+git clone https://github.com/b3-v3r/Hunner.git
+cd Hunner
+chmod +x hunner.py
+python hunner.py
+fi
+if [ $gg = 44 ] || [ $gg = 44 ]
+then
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd Nethunter-In-Termux
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+./startkali.sh
+fi
+if [ $gg = 45 ] || [ $gg = 45 ]
+then
+pkg install git
+git clone https://github.com/SirKz0L/l4efy-passtoolsv1.git
+cd l4efy-passtoolsv1
+chmod 777 Sir-Kz0L.sh
+./Sir-Kz0L
+fi
+if [ $gg = 46 ] || [ $gg = 46 ]
+then
+apt-get update && apt-get upgrade
+pkg install python
+apt-get install git
+git clone https://github.com/Manisso/fsociety
+cd fsociety
+python fsociety.py
+fi
+if [ $gg = 47 ] || [ $gg = 47 ]
+then
+git clone https://github.com/Xeit666h05t/CrewBot
+cd CrewBot
+python2 CrewBot.pyc
+fi
+if [ $gg = 48 ] || [ $gg = 48 ]
+then
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+fi
+if [ $gg = 49 ] || [ $gg = 49 ]
+then
+pip install scapy
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifiphisher
+python setup.py install
+cd wifiphisher
+python wifiphisher
+fi
+if [ $gg = 50 ] || [ $gg = 50 ]
+then
+curl -S https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --output ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+./ngrok -h
+./ngrok http 8080
+./ngrok tcp 4444
+fi
+if [ $gg = 51 ] || [ $gg = 51 ]
+then
+git clone https://github.com/adidoank/kubik
+cd kubik
+nano cfg.php
+php bot.php
+fi
+if [ $gg = 52 ] || [ $gg = 52 ]
+then
+pkg update && pkg upgrade
+pkg install curl
+pkg install python
+pkg install python2
+curl -LO 
+https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master/metasploitTechzindia.sh
+chmod 777 metasploitTechzindia.sh
+cd metasploit-framework
+./msfconsole
+fi
+if [ $gg = 00 ] || [ $gg = 00 ]
+then
+echo "\033[32;1m      BIKIN TOOLS INSTALLER ITU GAMPANG NJING"|lolcat
+sleep 1
+echo "\033[33;1m      SILAHKAN DI RECODE"|lolcat
+sleep 1
+echo "\033[32;1mTHANKS SUDAH GUNAKAN TOOLS SAYA"|lolcat
+figlet "MR.UNKNOW"|lolcat
+sleep 1
+exit
+fi
